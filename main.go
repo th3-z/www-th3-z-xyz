ebffc4813f5c562a22b2c6230e0940ae31dfe1ee
the_z <the_z@znel.org>
2019-12-15 14:23:10 +0000

    Recursive templates, restructure

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"github.com/labstack/echo"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"./database"
 	"./handlers"
@@ -13,15 +17,46 @@ type Template struct {
 	templates *template.Template
 }
 
+func findAndParseTemplates(rootDir string, funcMap template.FuncMap) (*template.Template, error) {
+	cleanRoot := filepath.Clean(rootDir)
+	pfx := len(cleanRoot)+1
+	root := template.New("")
+
+	err := filepath.Walk(cleanRoot, func(path string, info os.FileInfo, e1 error) error {
+		if !info.IsDir() && strings.HasSuffix(path, ".html") {
+			if e1 != nil {
+				return e1
+			}
+
+			b, e2 := ioutil.ReadFile(path)
+			if e2 != nil {
+				return e2
+			}
+
+			name := path[pfx:]
+			t := root.New(name).Funcs(funcMap)
+			t, e2 = t.Parse(string(b))
+			if e2 != nil {
+				return e2
+			}
+		}
+
+		return nil
+	})
+
+	return root, err
+}
+
 func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 	return t.templates.ExecuteTemplate(w, name, data)
 }
 
 func routes(e *echo.Echo) {
-	e.GET("/hello", handlers.Index)
+	e.GET("/", handlers.Index)
+	e.GET("/articles", handlers.Articles)
 
-	e.Static("/styles", "public/styles")
-	e.Static("/scripts", "public/scripts")
+	e.Static("/styles", "static/styles")
+	e.Static("/scripts", "static/scripts")
 }
 
 func main() {
@@ -29,7 +64,7 @@ func main() {
 	database.Migrate(db)
 
 	t := &Template{
-		templates: template.Must(template.ParseGlob("public/views/*.html")),
+		templates: template.Must(findAndParseTemplates("templates", nil)),
 	}
 
 	e := echo.New()
